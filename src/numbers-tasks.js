1979f2ba5dfd83d1d1a1a4c3c3b2a88f76d044a7
valeriya kuzn <valeriyakuznetsova123@gmail.com>
2023-11-13 13:18:53 +0300

    feat: add task 13

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -252,8 +252,14 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (!value) {
+    return def;
+  }
+  if (Number(value)) {
+    return Number(value);
+  }
+  return def;
 }
 
 /**
